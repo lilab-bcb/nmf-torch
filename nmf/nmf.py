2b3b12e6656099a7dec172f3fab4786e9b2df778
Yiming Yang <yang.yihming@gmail.com>
2021-02-08 23:53:16 -0500

    nndsvd init

--- nmf/nmf.py
+++ nmf/nmf.py
@@ -3,7 +3,7 @@ import random
 import torch
 
 class NMF:
-    def __init__(self, n_components, loss='l2', beta = 2, max_iter=200, tol=1e-4, random_state=0, fp_precision='float', use_gpu=False):
+    def __init__(self, n_components, init='nndsvd', loss='l2', beta = 2, max_iter=200, tol=1e-4, random_state=0, fp_precision='float', use_gpu=False):
         self.k = n_components
 
         if loss == 'l2':
@@ -30,25 +30,27 @@ class NMF:
             else:
                 print("CUDA is not available on your machine. Use CPU mode instead.")
 
+        self._init_method = init
         self._max_iter = max_iter
         self._tol = tol
         self._random_state = random_state
         self._deviance = []
 
-    def _loss(self, X_hat):
-        if self._beta == 0:
-            x_div_x_hat = self.X / X_hat
-            diver_mat = x_div_x_hat - torch.log2(x_div_x_hat) - 1
-        elif self._beta == 1:
-            diver_mat = self.X * torch.log2(self.X / X_hat) - self.X + X_hat
+    @staticmethod
+    def _loss(X, Y, beta):
+        if beta == 0:
+            x_div_y = X / Y
+            diver_mat = x_div_y - x_div_y.log2() - 1
+        elif beta == 1:
+            diver_mat = X * torch.log2(X / Y) - X + Y
         else:
-            coef = 1 / (self._beta * (self._beta - 1))
-            diver_mat = coef * (self.X.pow(self._beta) + (self._beta - 1) * X_hat.pow(self._beta) - self._beta * self.X * X_hat.pow(self._beta - 1))
+            coef = 1 / (beta * (beta - 1))
+            diver_mat = coef * (X.pow(beta) + (beta - 1) * Y.pow(beta) - beta * X * Y.pow(beta - 1))
         
-        return torch.sum(diver_mat)
+        return diver_mat.sum()
 
     def _update_loss(self, X_hat):
-        self._deviance.append(self._loss(X_hat))
+        self._deviance.append(self._loss(self.X, X_hat, self._beta))
     
     def _is_converged(self):
         if len(self._deviance) > 1:
@@ -59,11 +61,40 @@ class NMF:
     def _get_X_hat(self):
         return self.H @ self.W
 
-    def _initialize_W_H(self):
+    def _initialize_W_H(self, eps=1e-6):
         random.seed(self._random_state)
 
-        self.H = torch.rand((self.X.shape[0], self.k), dtype=self._tensor_dtype, device=self._device_type)
-        self.W = torch.rand((self.k, self.X.shape[1]), dtype=self._tensor_dtype, device=self._device_type)
+        if self._init_method == 'nndsvd':
+            U, S, V = torch.svd_lowrank(self.X, q=self.k)
+            H, W = torch.zeros_like(U), torch.zeros_like(V.T)
+            H[:, 0] = S[0].square() * U[:, 0]
+            W[0, :] = S[0].square() * V[:, 0]
+
+            for j in range(2, self.k):
+                x, y = U[:, j], V[:, j]
+                x_p, y_p = x.maximum(torch.zeros_like(x)), y.maximum(torch.zeros_like(y))
+                x_n, y_n = x.minimum(torch.zeros_like(x)).abs(), y.minimum(torch.zeros_like(y)).abs()
+                x_p_nrm, y_p_nrm = x_p.norm(p=2), y_p.norm(p=2)
+                x_n_nrm, y_n_nrm = x_n.norm(p=2), y_n.norm(p=2)
+                m_p, m_n = x_p_nrm * y_p_nrm, x_n_nrm * y_n_nrm
+
+                if m_p > m_n:
+                    u, v, sigma = x_p / x_p_nrm, y_p / y_p_nrm, m_p
+                else:
+                    u, v, sigma = x_n / x_n_nrm, y_n / y_n_nrm, m_n
+                
+                factor = (S[j] * sigma).sqrt()
+                H[:, j] = factor * u
+                W[j, :] = factor * v
+
+            H[H < eps] = 0
+            W[W < eps] = 0
+        else:
+            H = torch.rand((self.X.shape[0], self.k), dtype=self._tensor_dtype, device=self._device_type)
+            W = torch.rand((self.k, self.X.shape[1]), dtype=self._tensor_dtype, device=self._device_type)
+        
+        self.H = H
+        self.W = W
         self._update_loss(self._get_X_hat())
 
     @torch.no_grad()
@@ -104,5 +135,7 @@ class NMF:
             if i == self._max_iter - 1:
                 self.num_iters = self._max_iter
                 print(f"    Not converged after {self._max_iter} iteration(s).")
-
-        return self.W
+    
+    def fit_transform(self, X):
+        self.fit(X)
+        return self.H
--- test.py
+++ test.py
@@ -9,19 +9,27 @@ import matplotlib.pyplot as plt
 
 X = np.random.rand(4000, 2000)
 
-model1 = sd.NMF(n_components=12, init='random', beta_loss='kullback-leibler', tol=1e-4, max_iter=200, random_state=0, solver='mu')
-model2 = nmf.NMF(n_components=12, loss='l2', tol=1e-5, max_iter=200, random_state=0)
+#model1 = sd.NMF(n_components=12, init='random', beta_loss='kullback-leibler', tol=1e-4, max_iter=200, random_state=0, solver='mu')
+model2 = nmf.NMF(n_components=12, init='random', loss='l2', tol=1e-4, max_iter=200, random_state=0)
+model3 = nmf.NMF_alt(torch.tensor(X).T, rank=12, max_iterations=200, tolerance=1e-4, init_method='random')
 
-ts1_start = time.time()
-W1 = model1.fit_transform(X)
-ts1_end = time.time()
-print(f"Sklearn uses {ts1_end - ts1_start} s, with reconstruction error {model1.reconstruction_err_} after {model1.n_iter_} iteration(s).")
+#ts1_start = time.time()
+#W1 = model1.fit_transform(X)
+#ts1_end = time.time()
+#print(f"Sklearn uses {ts1_end - ts1_start} s, with reconstruction error {model1.reconstruction_err_} after {model1.n_iter_} iteration(s).")
 
 ts2_start = time.time()
-W2 = model2.fit(X)
+Y = model2.fit_transform(X)
 ts2_end = time.time()
-print(f"Pytorch uses {ts2_end - ts2_start} s, with final loss at {model2._deviance[-1]}.")
+print(f"Pytorch uses {ts2_end - ts2_start} s, with final loss at {model2._deviance[-1]} after {model2.num_iters} iteration(s).")
+print(model2._deviance)
 
 fig = plt.figure()
 ax = plt.plot(np.arange(model2.num_iters+1), model2._deviance, 'b-')
 fig.savefig("deviance.pdf", dpi=300)
+
+#ts3_start = time.time()
+#model3.fit(beta=2)
+#ts3_end = time.time()
+#err3 = nmf.NMF._loss(model3._V, model3.reconstruction, beta=2)
+#print(f"NMF_CPU uses {ts3_end - ts3_start} s, with final loss at {err3}.")
