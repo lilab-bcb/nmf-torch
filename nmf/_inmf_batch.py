6c7fca2f47d5bfdc289e2faedd25959b074adbc1
Bo Li <bli28@mgh.harvard.edu>
2021-03-13 17:23:23 -0500

    Updated inmf batch

--- nmf/_inmf_batch.py
+++ nmf/_inmf_batch.py
@@ -34,49 +34,42 @@ class INMFBatch:
 
     def _initialize_W_H_V(self):
         # Random initialization
-        avg = torch.sqrt(self.X[0].mean() / self._n_components)
-        self.W = torch.abs(avg * torch.randn((self._n_components, self._n_features), dtype=self._tensor_dtype, device=self._device_type))
-        W_t = self.W.T
-        self.H = []
-        self.V = []
-        init_err = self._SSX.clone()
-        self._H_t = []
-        self._HTH = []
-        self._WVWVT = []
-        self._XWVT = []
+        W = torch.zeros((self._n_components, self._n_features), dtype=self._tensor_dtype, device=self._device_type)
         for k in range(self._n_batches):
             avg = torch.sqrt(self.X[k].mean() / self._n_components)
             H = torch.abs(avg * torch.randn((self.X[k].shape[0], self._n_components), dtype=self._tensor_dtype, device=self._device_type))
-            V = torch.abs(avg * torch.randn((self._n_components, self._n_features), dtype=self._tensor_dtype, device=self._device_type))
+            V = torch.abs(0.5 * avg * torch.randn((self._n_components, self._n_features), dtype=self._tensor_dtype, device=self._device_type))
             self.H.append(H)
             self.V.append(V)
+            W += torch.abs(0.5 * avg * torch.randn((self._n_components, self._n_features), dtype=self._tensor_dtype, device=self._device_type))
+        W /= self._n_batches
 
+        self._HTH = []
+        self._WVWVT = []
+        self._XWVT = []
+        self._VVT = []
+        for k in range(self._n_batches):
             # Cache for batch update
             WV = self.W + V
-            WV_t = WV.T
-            self._H_t.append(H.T)
-            self._HTH.append(self._H_t[k] @ H)
-            self._WVWVT.append(WV @ WV_t)
-            self._XWVT.append(self.X[k] @ WV_t)
-
-            # Calculate loss of this batch
-            init_err += self._loss(batch_id=k)
-        self._init_err = torch.sqrt(init_err)
+            self._HTH.append(self.H[k].T @ self.H[k])
+            WV = self.W + self.V[k]
+            self._WVWVT.append(WV @ WV.T)
+            self._XWVT.append(self.X[k] @ WV.T)
+            if self._lambda > 0.0:
+                self._VVT.append(self.V[k] @ self.V[k].T)
+        self._init_err = self._loss()
         self._prev_err = self._init_err
         self._cur_err = self._init_err
 
 
-    def _loss(self, batch_id=None):
-        if batch_id is None:
-            res = self._SSX.clone()
-            for k in range(self._n_batches):
-                res += torch.trace(self._HTH[k] @ self._WVWVT[k]) - 2 * torch.trace(self._H_t[k] @ self._XWVT[k])
-                if self._lambda > 0:
-                    res += torch.sum((self.H[k]@self.V[k])**2)
-            return torch.sqrt(res)
-        else:
-            assert batch_id >= 0 and batch_id < self._n_batches, "Batch ID is out of bound!"
-            return torch.trace(self._HTH[batch_id] @ self._WVWVT[batch_id]) - 2 * torch.trace(self._H_t[batch_id] @ self._XWVT[batch_id])
+    def _loss(self):
+        res = 0.0
+        for k in range(self._n_batches):
+            res += torch.trace(self._HTH[k] @ self._WVWVT[k]) - 2 * torch.trace(self._H_t[k] @ self._XWVT[k])
+            if self._lambda > 0:
+                res += torch.trace(self._VVT[k] @ self._HTH[k])
+        res += self._SSX
+        return torch.sqrt(res)
 
 
     @property
@@ -93,40 +86,38 @@ class INMFBatch:
         mat[denom < self._epsilon] = 0.0
 
 
-    def _update_W(self):
+    def _update_H_V_W(self):
         W_numer = torch.zeros_like(self.W)
         W_denom = torch.zeros_like(self.W)
+        # Update Hs and Vs and calculate partials sum for updating W
         for k in range(self._n_batches):
-            W_numer += self._H_t[k] @ self.X[k]
-            W_denom += self._HTH[k] @ (self.W + self._lambda * self.V[k])
-        self._update_matrix(self.W, W_numer, W_denom)
+            # Update H[k]
+            H_numer = self._XWVT[k]
+            H_denom = self.H[k] @ self._WVWVT[k]
+            if self._lambda > 0:
+                H_denom += self._lambda * (self.H[k] @ self._VVT[k])
+            self._update_matrix(self.H[k], H_numer, H_denom)
+            # Cache HTH
+            self._HTH[k] = self.H[k].T @ self.H[k]
 
-
-    def _update_H_V(self):
-        for k in range(self._n_batches):
             # Update V[k]
-            V_numer = self._H_t[k] @ self.X[k]
+            V_numer = self.H[k].T @ self.X[k]
             V_denom = self._HTH[k] @ (self.W + self.V[k])
             if self._lambda > 0:
                 V_denom += self._lambda * (self._HTH[k] @ self.V[k])
             self._update_matrix(self.V[k], V_numer, V_denom)
 
-            # Cache WVWVT and XWVT
+            # Update W numer and denomer
+            W_numer += V_numer
+            W_denom += self._HTH[k] @ (self.W + self.V[k])
+        # Update W
+        self._update_matrix(self.W, W_numer, W_denom)
+        # Cache WVWVT and XWVT
+        for k in range(self._n_batches):
             WV = self.W + self.V[k]
-            WV_t = WV.T
-            self._WVWVT[k] = WV @ WV_t
-            self._XWVT[k] = self.X[k] @ WV_t
+            self._WVWVT[k] = WV @ WV.T
+            self._XWVT[k] = self.X[k] @ WV.T
 
-            # Update H[k]
-            H_numer = self._XWVT[k].clone()
-            H_denom = self.H[k] @ self._WVWVT[k]
-            if self._lambda > 0:
-                H_denom += self._lambda * (self.H[k] @ (self.V[k] @ self.V[k].T))
-            self._update_matrix(self.H[k], H_numer, H_denom)
-
-            # Cache H_t and HTH
-            self._H_t[k] = self.H[k].T
-            self._HTH[k] = self._H_t[k] @ self.H[k]
 
     def fit(
         self,
@@ -162,8 +153,7 @@ class INMFBatch:
                 else:
                     self._prev_err = self._cur_err
 
-            self._update_W()
-            self._update_H_V()
+            self._update_H_V_W()
 
             if i == self._max_iter - 1:
                 self.num_iters = self._max_iter
