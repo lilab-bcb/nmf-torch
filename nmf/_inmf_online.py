b7713a3e31a6a57d62d7915512627d19273b8a05
Bo Li <bli28@mgh.harvard.edu>
2021-03-18 15:33:32 -0400

    Update inmf online

--- nmf/_inmf_online.py
+++ nmf/_inmf_online.py
@@ -16,6 +16,7 @@ class INMFOnline(INMFBase):
         max_pass: int = 10,
         chunk_size: int = 2000,
         w_max_iter: int = 200,
+        v_max_iter: int = 50,
         h_max_iter: int = 50,
     ):
         super().__init__(
@@ -31,154 +32,215 @@ class INMFOnline(INMFBase):
         self._max_pass = max_pass
         self._chunk_size = chunk_size
         self._w_max_iter = w_max_iter
+        self._v_max_iter = v_max_iter
         self._h_max_iter = h_max_iter
 
 
-    def _h_err(self, h, WVWVT, xWVT, VVT=None, cache_hth=None):
-        hth = h.T @ h
-        if cache_hth is not None:
-            cache_hth[0] = hth
-        # Forbenious-norm^2 in trace format (No X)
-        res = torch.trace(WVWVT @ hth) - 2.0 * torch.trace(h.T @ xWVT)
-        # Add regularization terms if needed
-        if self._lambda > 0.0:
-            res += self._lambda * torch.trace(hth @ VVT)
+    def _h_err(self, h, hth, WVWVT, xWVT, VVT):
+        res = torch.trace((WVWVT + self._lambda * VVT) @ hth) if self._lambda > 0.0 else torch.trace(WVWVT @ hth) # Forbenious-norm^2 in trace format (No X)
+        res -= 2.0 * torch.trace(h.T @ xWVT)
         return res
 
 
-    def _WV_err(self, A, B, V, WV=None, WVWVT=None, VVT=None):
-        if WV is None:
-            WV = self.W + V
-            WVWVT = WV @ WV.T
-            VVT = V @ V.T
+    def _v_err(self, A, B, WV, WVWVT, VVT):
+        res = torch.trace((WVWVT + self._lambda * VVT) @ A) if self._lambda > 0.0 else torch.trace(WVWVT @ A)
+        res -= 2.0 * torch.trace(B @ WV.T)
+        return res
 
-        res = torch.trace(WVWVT @ A) - 2.0 * torch.trace(B @ WV.T)
-        # Add regularization terms if needed
-        if self._lambda > 0:
-            res += self._lambda * torch.trace(A @ VVT)
+
+    def _w_err(self, CW, E, D):
+        res = torch.trace((CW + 2.0 * E) @ self.W.T) - 2.0 * torch.trace(D @ self.W.T)
         return res
 
 
     def _update_one_pass(self):
-        W_factor_numer = torch.zeros((self._n_components, self._n_features), dtype=self._tensor_dtype, device=self._device_type)
-        A = [torch.zeros((self._n_components, self._n_components), dtype=self._tensor_dtype, device=self._device_type) for _ in range(self._n_batches)]
-        B = [torch.zeros((self._n_components, self._n_features), dtype=self._tensor_dtype, device=self._device_type) for _ in range(self._n_batches)]
-        # C is sum of B
-        C = torch.zeros_like(B[0])
-        # D is sum of AV
-        D = torch.zeros_like(B[0])
-        # E is sum of A
-        E = torch.zeros_like(A[0])
-
+        """
+            A = sum hth; B = sum htx; for each batch
+            C = sum of hth; D = sum of htx; E = sum of AV; for all batches
+        """
+        A = torch.zeros((self._n_components, self._n_components), dtype=self._tensor_dtype, device=self._device_type)
+        B = torch.zeros((self._n_components, self._n_features), dtype=self._tensor_dtype, device=self._device_type)
+        C = torch.zeros((self._n_components, self._n_components), dtype=self._tensor_dtype, device=self._device_type)
+        D = torch.zeros((self._n_components, self._n_features), dtype=self._tensor_dtype, device=self._device_type)
+        E = torch.zeros((self._n_components, self._n_features), dtype=self._tensor_dtype, device=self._device_type)
+        
         batch_indices = torch.randperm(self._n_batches, device=self._device_type)
         for k in batch_indices:
             indices = torch.randperm(self.X[k].shape[0], device=self._device_type)
 
-            # Block-wise update.
+            # Block-wise update
             i = 0
-            num_processed = 0
+            VVT = self.V[k] @ self.V[k].T if self._lambda > 0.0 else None
+            A.fill_(0.0)
+            B.fill_(0.0)
             while i < indices.shape[0]:
                 idx = indices[i:(i+self._chunk_size)]
-                cur_chunksize = idx.shape[0]
                 x = self.X[k][idx, :]
                 h = self.H[k][idx, :]
 
-                # Update H.
+                # Update H
                 WV = self.W + self.V[k]
                 WVWVT = WV @ WV.T
+                hth = h.T @ h
                 xWVT = x @ WV.T
-                VVT = self.V[k] @ self.V[k].T
 
                 h_factor_numer = xWVT
-
-                cache_hth = [None]
-                cur_h_err = self._h_err(h, WVWVT, xWVT, VVT, cache_hth)
+                cur_h_err = self._h_err(h, hth, WVWVT, xWVT, VVT)
 
                 for j in range(self._h_max_iter):
                     prev_h_err = cur_h_err
 
-                    h_factor_denom = h @ WVWVT
-                    if self._lambda:
-                        h_factor_denom += self._lambda * (h @ VVT)
+                    h_factor_denom = h @ (WVWVT + self._lambda * VVT) if self._lambda > 0.0 else h @ WVWVT
                     self._update_matrix(h, h_factor_numer, h_factor_denom)
-                    cur_h_err = self._h_err(h, WVWVT, xWVT, VVT, cache_hth)
+                    hth = h.T @ h
+                    cur_h_err = self._h_err(h, hth, WVWVT, xWVT, VVT)
 
                     if self._is_converged(prev_h_err, cur_h_err, prev_h_err):
                         break
 
                 self.H[k][idx, :] = h
 
-                # Update sufficient statistics A and B.
-                hth = cache_hth[0]
-                num_after = num_processed + cur_chunksize
+                # Update sufficient statistics for batch k
+                A += hth
+                htx = h.T @ x
+                B += htx
 
-                A[k] *= num_processed
-                A[k] += hth
-                A[k] /= num_after
+                # Update V
+                V_factor_numer = B
+                cur_v_err = self._v_err(A, B, WV, WVWVT, VVT)
 
-                B[k] *= num_processed
-                B[k] += h.T @ x
-                B[k] /= num_after
+                for j in range(self._v_max_iter):
+                    prev_v_err = cur_v_err
 
-                num_processed = num_after
+                    v_factor_denom = A @ (WV + self._lambda * self.V[k])
+                    self._update_matrix(self.V[k], v_factor_numer, v_factor_denom)
+                    WV = self.W + self.V[k]
+                    WVWVT = WV @ WV.T
+                    VVT = self.V[k] @ self.V[k].T if self._lambda > 0.0 else None
+                    cur_v_err = self._v_err(A, B, WV, WVWVT, VVT)
 
-                # Update V and W.
-                V_factor_numer = B[k]
-                W_factor_numer = C + B[k]
-
-                cur_WV_err = self._WV_err(A[k], B[k], self.V[k], WV, WVWVT, VVT)
-
-                for j in range(self._w_max_iter):
-                    prev_WV_err = cur_WV_err
-
-                    V_factor_denom = A[k] @ (self.W + (1 + self._lambda) * self.V[k])
-                    self._update_matrix(self.V[k], V_factor_numer, V_factor_denom)
-
-                    W_factor_denom = D + E @ self.W + A[k] @ (self.W + self.V[k])
-                    self._update_matrix(self.W, W_factor_numer, W_factor_denom)
-
-                    cur_WV_err = self._WV_err(A[k], B[k], self.V[k])
-
-                    if self._is_converged(prev_WV_err, cur_WV_err, prev_WV_err):
+                    if self._is_converged(prev_v_err, cur_v_err, prev_v_err):
                         break
 
-                # Update statistics of W.
-                C += B[k]
-                D += A[k] @ self.V[k]
-                E += A[k]
+                # Update sufficient statistics for all batches
+                C += hth
+                D += htx
+                CW = C @ self.W
+                E_new = E + A @ self.V[k]
+                
+                # Update W
+                W_factor_numer = D
+                cur_w_err = self._w_err(CW, E_new, D)
+                for j in range(self._w_max_iter):
+                    prev_w_err = cur_w_err
+
+                    W_factor_denom = CW + E_new
+                    self._update_matrix(self.W, W_factor_numer, W_factor_denom)
+                    CW = C @ self.W
+                    cur_WV_err = self._w_err(CW, E_new, D)
+
+                    if self._is_converged(prev_w_err, cur_w_err, prev_w_err):
+                        break
 
                 i += self._chunk_size
+            E = E_new
 
 
-
-    def _update_H(self):
+    def _update_H_V(self):
+        """
+            Fix W, only update V and H
+            A = sum hth; B = sum htx; for each batch
+        """
+        A = torch.zeros((self._n_components, self._n_components), dtype=self._tensor_dtype, device=self._device_type)
+        B = torch.zeros((self._n_components, self._n_features), dtype=self._tensor_dtype, device=self._device_type)
+        
         for k in range(self._n_batches):
+            indices = torch.randperm(self.X[k].shape[0], device=self._device_type)
+
+            # Block-wise update
+            i = 0
             WV = self.W + self.V[k]
             WVWVT = WV @ WV.T
+            VVT = self.V[k] @ self.V[k].T if self._lambda > 0.0 else None
+            A.fill_(0.0)
+            B.fill_(0.0)
+            while i < indices.shape[0]:
+                idx = indices[i:(i+self._chunk_size)]
+                x = self.X[k][idx, :]
+                h = self.H[k][idx, :]
 
-            i = 0
-            sum_h_err = 0.
-            while i < self.H[k].shape[0]:
-                x = self.X[k][i:(i+self._chunk_size), :]
-                h = self.H[k][i:(i+self._chunk_size), :]
-
+                # Update H
+                hth = h.T @ h
                 xWVT = x @ WV.T
+
                 h_factor_numer = xWVT
-
-                VVT = None
-                if self._lambda > 0:
-                    VVT = self.V[k] @ self.V[k].T
-
-                cur_h_err = self._h_err(h, WVWVT, xWVT, VVT)
+                cur_h_err = self._h_err(h, hth, WVWVT, xWVT, VVT)
 
                 for j in range(self._h_max_iter):
                     prev_h_err = cur_h_err
 
-                    h_factor_denom = h @ WVWVT
-                    if self._lambda > 0:
-                        h_factor_denom += self._lambda * (h @ VVT)
+                    h_factor_denom = h @ (WVWVT + self._lambda * VVT) if self._lambda > 0.0 else h @ WVWVT
                     self._update_matrix(h, h_factor_numer, h_factor_denom)
-                    cur_h_err = self._h_err(h, WVWVT, xWVT, VVT)
+                    hth = h.T @ h
+                    cur_h_err = self._h_err(h, hth, WVWVT, xWVT, VVT)
+
+                    if self._is_converged(prev_h_err, cur_h_err, prev_h_err):
+                        break
+
+                self.H[k][idx, :] = h
+
+                # Update sufficient statistics for batch k
+                A += hth
+                htx = h.T @ x
+                B += htx
+
+                # Update V
+                V_factor_numer = B
+                cur_v_err = self._v_err(A, B, WV, WVWVT, VVT)
+
+                for j in range(self._v_max_iter):
+                    prev_v_err = cur_v_err
+
+                    v_factor_denom = A @ (WV + self._lambda * self.V[k])
+                    self._update_matrix(self.V[k], v_factor_numer, v_factor_denom)
+                    WV = self.W + self.V[k]
+                    WVWVT = WV @ WV.T
+                    VVT = self.V[k] @ self.V[k].T if self._lambda > 0.0 else None
+                    cur_v_err = self._v_err(A, B, WV, WVWVT, VVT)
+
+                    if self._is_converged(prev_v_err, cur_v_err, prev_v_err):
+                        break
+
+                i += self._chunk_size
+
+
+    def _update_H(self):
+        """ Fix W and V, update H """
+        for k in range(self._n_batches):
+            WV = self.W + self.V[k]
+            WVWVT = WV @ WV.T
+            VVT = self.V[k] @ self.V[k].T if self._lambda > 0.0 else None
+
+            i = 0
+            sum_h_err = 0.0
+            while i < self.H[k].shape[0]:
+                x = self.X[k][i:(i+self._chunk_size), :]
+                h = self.H[k][i:(i+self._chunk_size), :]
+
+                # Update H
+                hth = h.T @ h
+                xWVT = x @ WV.T
+
+                h_factor_numer = xWVT
+                cur_h_err = self._h_err(h, hth, WVWVT, xWVT, VVT)
+
+                for j in range(self._h_max_iter):
+                    prev_h_err = cur_h_err
+
+                    h_factor_denom = h @ (WVWVT + self._lambda * VVT) if self._lambda > 0.0 else h @ WVWVT
+                    self._update_matrix(h, h_factor_numer, h_factor_denom)
+                    hth = h.T @ h
+                    cur_h_err = self._h_err(h, hth, WVWVT, xWVT, VVT)
 
                     if self._is_converged(prev_h_err, cur_h_err, prev_h_err):
                         break
@@ -206,8 +268,7 @@ class INMFOnline(INMFBase):
 
         for i in range(self._max_pass):
             self._update_one_pass()
-
-            # Update H.
+            self._update_H_V()
             H_err = self._update_H()
 
             self._cur_err = torch.sqrt(H_err + self._SSX)
