8560a6434524c48690d7df4d7daad9b0711db750
Yiming Yang <yang.yihming@gmail.com>
2021-05-27 21:03:09 -0400

    Init cython implementation

--- nmf/__init__.py
+++ nmf/__init__.py
@@ -8,3 +8,15 @@ from ._inmf_batch_hals import INMFBatchHALS
 from ._inmf_batch_hals_wrong import INMFBatchHALSWrong
 from ._inmf_batch_nnls_bpp import INMFBatchNnlsBpp
 from ._inmf_online import INMFOnline
+from ._inmf_batch_nnls_bpp import INMFBatchNnlsBpp
+
+try:
+    from importlib.metadata import version, PackageNotFoundError
+except ImportError:  # < Python 3.8: Use backport module
+    from importlib_metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version('nmf-torch')
+    del version
+except PackageNotFoundError:
+    pass
